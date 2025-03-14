fa520b6873478b4261316f2322fa0a3fd082bddd
geodebreaker 2 <gziegler@madison.k12.wi.us>
2025-03-14 21:44:06 +0000

    saffa

--- svr/node.js
+++ svr/node.js
@@ -12,19 +12,33 @@ const server = process.argv[2] == '-d' ? http.createServer((req, res) => {
 }) : null;
 
 const wss = new WebSocket.Server(server ? { server } : { port: process.env.PORT });
-let newPersist = {
-  money: 200
+
+let newSvr = {
+  packets: {},
+  mapUD: [],
+  persist: {},
+  marker: null,
+  newPersist: {
+    money: 200,
+    pos: [0, 0, 0]
+  }
 };
 
-let packets = {};
-let mapUD = [];
-let persist = {};
-let marker = null;
+let svrs = {
+
+};
+
+function createSvr(name) {
+  svrs[name] = structuredClone(newSvr);
+}
+
+createSvr('g');
 
 wss.on("connection", ws => {
   console.log("connection");
   ws.mapUD = [];
   ws.chat = [];
+  ws.svr = Object.keys(svrs)[0];
 
   ws.on("message", x => {
     let msg = JSON.parse(x.toString());
@@ -37,7 +51,7 @@ wss.on("connection", ws => {
         if (ws.name) {
           let p = msg.packet;
           if (p.marker) {
-            marker = p.marker;
+            svrs[ws.svr].marker = p.marker;
           }
           if (p.chat) {
             wss.clients.forEach(x => {
@@ -48,11 +62,11 @@ wss.on("connection", ws => {
           if (p.mapUD) {
             p.mapUD.forEach(x => {
               if (x[0] == 'calc') {
-                let y = mapUD.find(y => y[1] == x[1] && y[2] == y[2]);
+                let y = svrs[ws.svr].mapUD.find(y => y[1] == x[1] && y[2] == y[2]);
                 if (y) y[3] = x[3];
-                else mapUD.push(x);
+                else svrs[ws.svr].mapUD.push(x);
               } else {
-                mapUD.push(x);
+                svrs[ws.svr].mapUD.push(x);
               }
             });
             wss.clients.forEach(x => {
@@ -62,24 +76,24 @@ wss.on("connection", ws => {
             delete p.mapUD;
           }
           if (p.persist) {
-            persist[ws.name] = p.persist;
+            svrs[ws.svr].persist[ws.name] = p.persist;
             delete p.persist;
           }
-          packets[ws.name] = msg.packet;
+          svrs[ws.svr].packets[ws.name] = msg.packet;
         }
         break;
       case 'join':
-        if (packets[msg.name] && !packets[msg.name].to) {
+        if (userOn(msg.name)) {
           send(ws, { type: 'fail', error: 'name taken' });
           return ws.close();
         }
         ws.name = msg.name;
         send(ws, {
           type: 'connected',
-          packets,
+          packets: svrs[ws.svr].packets,
           map: getMap(),
-          mapUD,
-          persist: persist[ws.name] || newPersist
+          mapUD: svrs[ws.svr].mapUD,
+          persist: svrs[ws.svr].persist[ws.name] || svrs[ws.svr].newPersist
         });
         wss.clients.forEach(x => {
           if (x.name && x != ws)
@@ -111,37 +125,41 @@ wss.on("connection", ws => {
           })
       });
     if (ws.name)
-      delete packets[ws.name];
+      delete svrs[ws.svr].packets[ws.name];
     console.log(ws.name, "disconnected");
   });
 });
 
 function send(ws, data) {
-  if (data.type != 'update') console.log(ws.name, '>', data);
+  if (data.type != 'update' && data.type != 'connected') console.log(ws.name, '>', data);
   if (ws.readyState === WebSocket.OPEN) {
     ws.send(JSON.stringify(data));
   }
 }
 
 setInterval(() => {
-  Object.entries(packets).forEach(packet => {
-    if (packet[1].t + 9e3 < Date.now())
-      packets[packet[0]].to = true;
+  [].concat(...Object.values(svrs).map(x => Object.values(x.packets))).forEach(packet => {
+    if (packet.t + 9e3 < Date.now())
+      packet.to = true;
   });
   wss.clients.forEach(x => {
     if (x.name) {
       send(x, {
         type: 'update',
-        packets,
+        packets: svrs[x.svr].packets,
         chat: x.chat,
         mapUD: x.mapUD,
-        marker
+        marker: svrs[x.svr].marker
       });
       x.chat = [];
       x.mapUD = [];
     }
   });
 }, 200);
+
+function userOn(u) {
+  return !Object.values(svrs).every(x => Object.entries(x.packets).every(y => y[0] != u || y[0].to));
+}
 
 const PORT = 8080;
 if (server) server.listen(PORT, () => {
@@ -153,7 +171,7 @@ function getMap() {
     //.replaceAll("***", Math.floor(Math.random() * (36 ** 8 - 1)).toString(36)) +
     fs.readdirSync('./game/').map(x => {
       // if (fs.fstatSync(2, './game/' + x).isFile())
-        try { return fs.readFileSync('./game/' + x).toString(); } catch(e) { return '' }
+      try { return fs.readFileSync('./game/' + x).toString(); } catch (e) { return '' }
       // else return 'console.log("error")';
     }).join('\r\n\r\n'));
 }
